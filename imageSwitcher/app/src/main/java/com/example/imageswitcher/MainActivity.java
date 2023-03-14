48cb24abd7feeb10df343f79463ffa07c5172910
KIM NA HYEON <hjna0206@naver.com>
2023-03-14 15:06:19 +0900

    이미지 스위처 정리

--- imageSwitcher/app/src/main/java/com/example/imageswitcher/MainActivity.java
+++ imageSwitcher/app/src/main/java/com/example/imageswitcher/MainActivity.java
@@ -7,16 +7,30 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import kotlin.random.Random;
-
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private ImageView mainImageView;
+    private TextView pageTextView;
+    private int page = 1;
+
+    private static final int [] IMG_RES_IDS = new int[]{
+            R.mipmap.cat_1,
+            R.mipmap.cat_2,
+            R.mipmap.cat_3,
+            R.mipmap.cat_4,
+            R.mipmap.cat_5,
+            R.mipmap.cat_6
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainImageView = findViewById(R.id.mainImageView);
+        pageTextView = findViewById(R.id.pageTextView);
     }
 
     public void onBtnPrev(View view) {
@@ -31,31 +45,16 @@ public class MainActivity extends AppCompatActivity {
         setPage(page+1);
     }
 
-    private int page = 1;
-
-    private int [] resIds = new int[]{
-            R.mipmap.cat_1,
-            R.mipmap.cat_2,
-            R.mipmap.cat_3,
-            R.mipmap.cat_4,
-            R.mipmap.cat_5,
-    };
-
-
 
     private void setPage(int page){
-        if(page <1 || page >5) return;
+        if(page <1 || page >IMG_RES_IDS.length) return;
+        int resId = IMG_RES_IDS[page-1];
 
-        int resId = resIds[page-1];
-        ImageView iv = findViewById(R.id.mainImageView);
-        iv.setImageResource(resId);
-
-        TextView tv= findViewById(R.id.pageTextView);
-        tv.setText(page+ " / 5");
+        mainImageView.setImageResource(resId);
+        pageTextView.setText(page+ " / "+ IMG_RES_IDS.length);
 
         this.page =page;
     }
-
 
 
 
