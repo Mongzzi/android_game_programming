c8875b0f86604eb20450643225d57161ba811bfd
KIM NA HYEON <hjna0206@naver.com>
2023-03-14 14:20:44 +0900

    0314

---
+++ imageSwitcher/app/src/main/java/com/example/imageswitcher/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.imageswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnPrev(View view) {
+        Log.d(TAG, "Prev pressed");
+    }
+
+    public void onBtnNext(View view) {
+        Log.d(TAG, "Next pressed");
+
+    }
+}
--- work1/app/src/main/java/com/example/myfirstapp_2018182004/MainActivity.java
+++ work1/app/src/main/java/com/example/myfirstapp_2018182004/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myfirstapp_2018182004;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);         /// R -> 리소스 의미 생성된 클래스 안드로이드 에디터에 의해서 r.java 생성 , 수정 불가
+
+    }
+
+
+    public void onBtnPushMe(View view){
+        TextView tv = findViewById(R.id.snumTextView);
+        tv.setText("Push me");
+    }
+
+
+    public void onBtnAnother(View view) {
+        TextView tv = findViewById(R.id.snumTextView);
+        tv.setText("Another");
     }
 }
